3210b22c3a80c1c0e05d5c792e51c0323f02e543
stream2000 <18889897088@163.com>
2020-05-29 10:08:16 +0800

    Refactor: replace explicit lock with synchronized key word

--- src/main/java/cn/minus4/blockchain/Miner.java
+++ src/main/java/cn/minus4/blockchain/Miner.java
@@ -11,7 +11,6 @@ public class Miner {
     private final List<Block> blockchain = new ArrayList<>();
     private final List<Miner> Peers;
     private final int me;
-    private final Lock lock = new ReentrantLock();
     private final BlockingQueue<Transaction> transactions = new LinkedBlockingQueue<>();
     private State state = State.Preparing;
     private Thread gatherTask;
@@ -83,22 +82,22 @@ public class Miner {
                         if (!verifyBlockChain(newBlockChain)) {
                             continue;
                         }
-                        lock.lock();
-                        currentLength = blockchain.size();
-                        if (newBlockChain.size() > currentLength) {
-                            blockchain.clear();
-                            blockchain.addAll(newBlockChain);
-                            //                        transactions.clear();
-                            switch (state) {
-                                case Mining:
-                                    mineTask.interrupt();
-                                    newGatherTask();
-                                case Preparing:
-                                    gatherTask.interrupt();
-                                    newGatherTask();
+                        synchronized (Miner.this){
+                            currentLength = blockchain.size();
+                            if (newBlockChain.size() > currentLength) {
+                                blockchain.clear();
+                                blockchain.addAll(newBlockChain);
+                                //                        transactions.clear();
+                                switch (state) {
+                                    case Mining:
+                                        mineTask.interrupt();
+                                        newGatherTask();
+                                    case Preparing:
+                                        gatherTask.interrupt();
+                                        newGatherTask();
+                                }
                             }
                         }
-                        lock.unlock();
                     }
                 } catch (InterruptedException e) {
                     return;
@@ -123,30 +122,29 @@ public class Miner {
             if (Thread.interrupted()) {
                 return;
             }
-            lock.lock();
-            if (state == State.Mining && getPreviousHash().endsWith(previousHash) && length == blockchain.size()) {
-                blockchain.add(block);
-                System.out.println("Thread " + me + " mined ");
-                System.out.println("current chain:");
-                for (int i = 0; i < blockchain.size(); i++) {
-                    System.out.println(blockchain.get(i).getHash());
-                }
-                checkTask.setCurrentLength(blockchain.size());
-                // broadcast
-                for (int i = 0; i < Peers.size(); i++) {
-                    if (i == me) {
-                        continue;
+            synchronized (Miner.this){
+                if (state == State.Mining && getPreviousHash().endsWith(previousHash) && length == blockchain.size()) {
+                    blockchain.add(block);
+                    System.out.println("Thread " + me + " mined ");
+                    System.out.println("current chain:");
+                    for (int i = 0; i < blockchain.size(); i++) {
+                        System.out.println(blockchain.get(i).getHash());
                     }
-                    try {
-                        Peers.get(i).sendNewBlockChain(blockchain);
-                    } catch (InterruptedException e) {
-                        lock.unlock();
-                        return;
+                    checkTask.setCurrentLength(blockchain.size());
+                    // broadcast
+                    for (int i = 0; i < Peers.size(); i++) {
+                        if (i == me) {
+                            continue;
+                        }
+                        try {
+                            Peers.get(i).sendNewBlockChain(blockchain);
+                        } catch (InterruptedException e) {
+                            return;
+                        }
                     }
+                    newGatherTask();
                 }
-                newGatherTask();
             }
-            lock.unlock();
         }
     }
 
@@ -167,19 +165,19 @@ public class Miner {
                     Transaction.verifySignature(newTransaction, newTransaction.getSignature());
                     localTransactions.add(newTransaction);
                     if (localTransactions.size() == 100) {
-                        lock.lock();
-                        if (state == State.Preparing) {
-                            if (previousHash.equals(getPreviousHash()) && length == blockchain.size()) {
-                                state = State.Mining;
-                                gatherTask = null;
-                                Block block = new Block(previousHash);
-                                block.setTransactions(localTransactions);
-                                Thread task = new Thread(new MineTask(block, previousHash, length));
-                                mineTask = task;
-                                task.start();
+                        synchronized (Miner.this){
+                            if (state == State.Preparing) {
+                                if (previousHash.equals(getPreviousHash()) && length == blockchain.size()) {
+                                    state = State.Mining;
+                                    gatherTask = null;
+                                    Block block = new Block(previousHash);
+                                    block.setTransactions(localTransactions);
+                                    Thread task = new Thread(new MineTask(block, previousHash, length));
+                                    mineTask = task;
+                                    task.start();
+                                }
                             }
                         }
-                        lock.unlock();
                         return;
                     }
                 } catch (InterruptedException e) {
